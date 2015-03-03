ec454a2395683f3a630b175f175f09b1f7248503
JavaLearner <slavapv2007@gmail.com>
2015-03-03 11:20:39 +0200

    coding

--- src/com/tictactoe/gui/game/gameframe/GameFrame.java
+++ src/com/tictactoe/gui/game/gameframe/GameFrame.java
@@ -1,11 +1,11 @@
 package com.tictactoe.gui.game.gameframe;
 
-import java.awt.*;
-
 /**
  * Created by JavaLearner on 02-Mar-15.
  */
-public class GameFrame extends Frame{
+public class GameFrame implements IGameFrame{
+    public GameFrame(Straing MainTitle){
 
+    }
 
 }
--- src/com/tictactoe/gui/game/gameframe/IGameFrame.java
+++ src/com/tictactoe/gui/game/gameframe/IGameFrame.java
@@ -1,8 +1,11 @@
 package com.tictactoe.gui.game.gameframe;
 
+import java.awt.*;
+
 /**
  * Created by JavaLearner on 02-Mar-15.
  */
-public interface IGameFrame {
+public interface IGameFrame{
+
 
 }
